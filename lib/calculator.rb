43ec4a97814f6bf53ae0928c16dd1b72337d957a
Al <al@Als-MacBook-Pro.local>
2018-10-31 13:23:56 +0000

    calculator can square a number and return the result

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,8 @@ class Calculator
   def multiply(num1, num2)
     num1 * num2
   end
+
+  def square(num)
+    multiply(num, num)
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,10 +1,13 @@
 require 'calculator'
 
 describe(Calculator) do
-  it { is_expected.to respond_to(:multiply) }
-
   describe('#multiply') do
     it { expect(subject.multiply(2,3)).to eq(6) }
     it { expect(subject.multiply(3,5)).to eq(15) }
+  end
+
+  describe('#square') do
+    it { expect(subject.square(9)).to eq(81) }
+    it { expect(subject.square(100)).to eq(10000) }
   end
 end
