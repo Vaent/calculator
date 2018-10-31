c85f11e909ef930f4645fe203e70f405eb7e6049
Al <al@Als-MacBook-Pro.local>
2018-10-31 13:32:26 +0000

    results are now interpolated into an output string

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,9 +1,13 @@
 class Calculator
   def multiply(num1, num2)
-    num1 * num2
+    output(num1 * num2)
   end
 
   def square(num)
     multiply(num, num)
+  end
+
+  def output(result)
+    "the answer is #{result}"
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,12 +2,12 @@ require 'calculator'
 
 describe(Calculator) do
   describe('#multiply') do
-    it { expect(subject.multiply(2,3)).to eq(6) }
-    it { expect(subject.multiply(3,5)).to eq(15) }
+    it { expect(subject.multiply(2,3)).to eq('the answer is 6') }
+    it { expect(subject.multiply(3,5)).to eq('the answer is 15') }
   end
 
   describe('#square') do
-    it { expect(subject.square(9)).to eq(81) }
-    it { expect(subject.square(100)).to eq(10000) }
+    it { expect(subject.square(9)).to eq('the answer is 81') }
+    it { expect(subject.square(100)).to eq('the answer is 10000') }
   end
 end
